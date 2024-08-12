4912859ec1e43509a71962897504648d053cab85
mxxyblue <mxxyblue@gmail.com>
2024-08-12 22:42:00 +0900

    주석 추가

--- src/main/java/calculator/com/calculator/controller/CalController.java
+++ src/main/java/calculator/com/calculator/controller/CalController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CalController {
 	
+	/**
+	 * 계산기 화면 출력
+	 * @return
+	 */
 	@RequestMapping(value = "/calculator", method = RequestMethod.GET)
 	public String goCalculator() {
 		return "calculator";
